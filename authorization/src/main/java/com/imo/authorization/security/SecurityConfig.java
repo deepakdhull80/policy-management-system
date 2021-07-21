4a826279d086a5cada270da7fa71733df8e5c4e9
deepakdhull80 <deepakdhull80@gmail.com>
2021-07-21 18:00:55 +0530

    Jwt token authentication implemented and zuul api gateway integrated in it.

---
+++ authorization/src/main/java/com/imo/authorization/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.imo.authorization.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	JwtTokenFilter jwtTokenFilter;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
+	@Override @Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean(); 
+	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		
+		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.cors().and()
+		.csrf().disable().authorizeRequests()
+				.antMatchers(HttpMethod.POST,"/auth/login").permitAll()
+				.antMatchers(HttpMethod.GET,"/user/**").hasAnyRole("USER","ADMIN")
+				.antMatchers(HttpMethod.POST,"/user/**").hasRole("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic();
+	}
+	
+	
+}
