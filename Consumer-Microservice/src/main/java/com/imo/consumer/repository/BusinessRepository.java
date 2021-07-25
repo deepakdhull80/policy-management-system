15d742da0874edf2a525c5ab089ae47f5cc7fb6e
deepakdhull80 <deepakdhull80@gmail.com>
2021-07-26 00:58:08 +0530

    clean

--- Consumer-Microservice/src/main/java/com/imo/consumer/repository/BusinessRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imo.consumer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.imo.consumer.model.BusinessDetails;
-
-public interface BusinessRepository extends JpaRepository<BusinessDetails,Long>{
-
-}
