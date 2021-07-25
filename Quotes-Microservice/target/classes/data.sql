47b385896e94918561bcffe053389775233b3abc
deepakdhull80 <deepakdhull80@gmail.com>
2021-07-26 01:01:26 +0530

    UI

---
+++ Quotes-Microservice/target/classes/data.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Quotes_Master ( 
+   ID INT NOT NULL, 
+   Business_Value INT NOT NULL, 
+   Property_Value INT NOT NULL,
+   Property_Type VARCHAR(20) NOT NULL, 
+   Quotes VARCHAR(20) NOT NULL
+);
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (1,0,0,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (2,0,1,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (3,0,2,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (4,1,0,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (5,1,1,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (6,1,2,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (7,2,0,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (8,2,1,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (9,2,2,'Factory Equipment','90,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (10,3,3,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (11,3,4,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (12,3,5,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (13,4,3,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (14,4,4,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (15,4,5,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (16,5,3,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (17,5,4,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (18,5,5,'Factory Equipment','60,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (19,6,6,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (20,6,7,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (21,6,8,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (22,7,6,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (23,7,7,'Factory Equipment','36,000 INR'); 
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (24,7,8,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (25,8,6,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (26,8,7,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (27,8,8,'Factory Equipment','36,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (28,9,9,'Factory Equipment','24,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (29,9,10,'Factory Equipment','24,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (30,10,9,'Factory Equipment','24,000 INR');
+INSERT INTO Quotes_Master(ID,Business_Value,Property_Value,Property_Type,Quotes) VALUES (31,10,10,'Factory Equipment','24,000 INR');
---
+++ authorization/HELP.md
@@ -0,0 +1,25 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.4.8/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.4.8/maven-plugin/reference/html/#build-image)
+* [Spring Security](https://docs.spring.io/spring-boot/docs/2.5.2/reference/htmlsingle/#boot-features-security)
+* [Eureka Discovery Client](https://docs.spring.io/spring-cloud-netflix/docs/current/reference/html/#service-discovery-eureka-clients)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/2.5.2/reference/htmlsingle/#boot-features-developing-web-applications)
+* [Spring Data JPA](https://docs.spring.io/spring-boot/docs/2.5.2/reference/htmlsingle/#boot-features-jpa-and-spring-data)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Securing a Web Application](https://spring.io/guides/gs/securing-web/)
+* [Spring Boot and OAuth2](https://spring.io/guides/tutorials/spring-boot-oauth2/)
+* [Authenticating a User with LDAP](https://spring.io/guides/gs/authenticating-ldap/)
+* [Service Registration and Discovery with Eureka and Spring Cloud](https://spring.io/guides/gs/service-registration-and-discovery/)
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/bookmarks/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+
